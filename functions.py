ff2a04c7ab0b1daaef698a86519789c35b673815
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-02 01:25:56 -0400

    custom aspect ratio support

---
+++ functions.py
@@ -0,0 +1,38 @@
+import struct
+import keystone
+from keystone import *
+import binascii
+import math
+import os
+
+def make_hex(x, r):
+    p = math.floor(math.log(x, 2))
+    a = round(16*(p-2) + x / 2**(p-4))
+    if a<0: a += 128
+    a = 2*a + 1
+    h = hex(a).lstrip('0x').rjust(2,'0').upper()
+    hex_value = f'0{r}' + h[1] + '02' + h[0] + '1E' 
+    print(hex_value)
+    return hex_value
+
+def asm_to_hex(asm_code):
+    ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN)
+    encoding, count = ks.asm(asm_code)
+    return ''.join('{:02x}'.format(x) for x in encoding)
+
+def mvdk_hex23(num):
+    num = round(num, 15)
+    packed = struct.pack('!f', num)
+    full_hex = ''.join('{:02x}'.format(b) for b in packed)
+    hex_1 = full_hex[:4]
+    hex_2 = full_hex[4:]
+    asm_1 = f"movz w10, #0x{hex_2}"
+    asm_2 = f"movk w10, #0x{hex_1}, lsl #16"
+    asm_3 = f"movz w8, #0x{hex_2}"
+    asm_4 = f"movk w8, #0x{hex_1}, lsl #16"
+    hex_value1 = asm_to_hex(asm_1)
+    hex_value2 = asm_to_hex(asm_2)
+    hex_value3 = asm_to_hex(asm_1)
+    hex_value4 = asm_to_hex(asm_2)
+    return hex_value1, hex_value2, hex_value3, hex_value4
+
--- patch.py
+++ patch.py
@@ -1,25 +1,16 @@
 import os
 import math
+from functions import *
 
 def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
-
-    def make_hex(x, r):
-        p = math.floor(math.log(x, 2))
-        a = round(16*(p-2) + x / 2**(p-4))
-        if a<0: a += 128
-        a = 2*a + 1
-        h = hex(a).lstrip('0x').rjust(2,'0').upper()
-        hex_value = f'0{r}' + h[1] + '02' + h[0] + '1E' 
-        print(hex_value)
-        return hex_value
 
     visual_fixesa = visual_fixes[0]
     visual_fixesb = visual_fixes[1]
     scaling_factor = float(scaling_factor)
     ratio_value = float(ratio_value)
     print(f"The scaling factor is {scaling_factor}.")
-    hex_value = make_hex(ratio_value, 0)
-    hex_value2 = make_hex(ratio_value, 3)
+    hex_value = make_hex(1.75, 0)
+    hex_value1, hex_value2, hex_value3, hex_value4 = mvdk_hex23(ratio_value)
     version_variables = ["1.0.0", "1.0.1"]
     for version_variable in version_variables:
         file_name = f"main-{version_variable}.pchtxt"
@@ -27,14 +18,10 @@ def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
 
         if version_variable == "1.0.0":
             nsobidid = "37CE685A99F0BC0FB72132C40015F101ECB81C46"
-            replacement_value = "008FADE0"
-            replacement2_value = "009692D0"
             visual_fix = visual_fixesa
 
         elif version_variable == "1.0.1":
             nsobidid = "43AE51EE9766867DA5F1272CA0D3D8A60AC53296"
-            replacement_value = "008FADE0"
-            replacement2_value = "009692D0"
             visual_fix = visual_fixesb
 
         patch_content = f'''@nsobid-{nsobidid}
@@ -43,19 +30,18 @@ def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
 @flag offset_shift 0x100
 
 @enabled
-0045fd90 CA719C52
-0045fd94 0A03A872
+0045fd90 {hex_value1}
+0045fd94 {hex_value2}
 00e71c20 1F2003D5
-0003ce68 00902F1E
+0003ce68 {hex_value}
 016b10d8 1F2003D5
-00e169c8 C8719C52
-00e169cc 0803A872
+00e169c8 {hex_value3}
+00e169cc {hex_value4}
 @stop
 
 {visual_fix}
 
 // Generated using MVDK-AAR by Fayaz (github.com/fayaz12g/mvdk-aar)
-// Non 21:9 AR coming soon
 // Made possible by Fl4sh_#9174'''
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w') as patch_file:
